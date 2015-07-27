bd16bf1bbd7dd5e0da7ebebe05f953457d8547c0
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-07-27 23:38:51 +0300

    finish list of trailers

--- app/src/main/java/com/ilyarudyak/android/popmovies/DetailActivityFragment.java
+++ app/src/main/java/com/ilyarudyak/android/popmovies/DetailActivityFragment.java
@@ -1,11 +1,9 @@
 package com.ilyarudyak.android.popmovies;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,17 +43,31 @@ public class DetailActivityFragment extends Fragment {
         TextView userRating = (TextView) rootView.findViewById(R.id.textViewUserRating);
         TextView plotSynopsis = (TextView) rootView.findViewById(R.id.textViewPlotSynopsis);
 
-//        addTrailers();
-        View trailerView = inflater.inflate(R.layout.trailer, container, false);
-        View divider = inflater.inflate(R.layout.divider, container, false);
-        mainLinearLayout.addView(trailerView);
-        mainLinearLayout.addView(divider);
-
+        // stage 2: add list of trailers
         ArrayList<Movie.Trailer> trailersList =  getActivity().getIntent()
                 .getParcelableArrayListExtra(Movie.TRAILER_LIST);
-        String url = trailersList.get(0).getTrailerPathAbsolute();
-        Log.i(LOG_TAG, trailersList.toString());
+        for(Movie.Trailer mt : trailersList) {
+            View trailerView = inflater.inflate(R.layout.trailer, container, false);
+            final String url = mt.getTrailerPathAbsolute();
 
+            ImageButton trailerButton = (ImageButton) trailerView
+                    .findViewById(R.id.trailer_image_button);
+            trailerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    openTrailer(url);
+                }
+            });
+
+            TextView trailerTitle = (TextView) trailerView
+                    .findViewById(R.id.trailer_title_text_view);
+            trailerTitle.setText(mt.getTrailerName());
+
+            mainLinearLayout.addView(trailerView);
+
+            View divider = inflater.inflate(R.layout.divider, container, false);
+            mainLinearLayout.addView(divider);
+        }
 
         // detail activity called via intent.
         // inspect the intent for data.
@@ -74,47 +86,6 @@ public class DetailActivityFragment extends Fragment {
                 .into(posterImageView);
 
         return rootView;
-    }
-
-    // stage 2 add trailers to details view
-    // we create and add them programmatically -
-    // each movie can have different number of trailers
-    private void addTrailers() {
-
-        LinearLayout trailerLinearLayout = new LinearLayout(getActivity());
-        mainLinearLayout.addView(trailerLinearLayout);
-
-        ImageButton trailerButton = new ImageButton(getActivity());
-        Drawable iconArrow = getActivity().getResources()
-                .getDrawable(R.drawable.ic_play_arrow_black_36dp);
-        trailerButton.setBackground(iconArrow);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        );
-        params.setMargins(48, 16, 16, 16);
-        trailerButton.setLayoutParams(params);
-        trailerLinearLayout.addView(trailerButton);
-
-        TextView trailerTextView = new TextView(getActivity());
-        trailerTextView.setText("Trailer");
-        trailerTextView.setTextSize(16);
-        params.setMargins(48, 24, 16, 16);
-        trailerTextView.setLayoutParams(params);
-        trailerLinearLayout.addView(trailerTextView);
-
-        trailerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                ArrayList<Movie.Trailer> trailersList =  getActivity().getIntent()
-                        .getParcelableArrayListExtra(Movie.TRAILER_LIST);
-                String url = trailersList.get(0).getTrailerPathAbsolute();
-                Log.i(LOG_TAG, url);
-                openTrailer(url);
-            }
-        });
-
     }
 
     private void openTrailer(String url) {
