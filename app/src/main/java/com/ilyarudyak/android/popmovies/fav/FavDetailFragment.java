b759667c47a2f287db39fdf4dc06f51c4627d2d3
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-08-11 16:34:48 +0300

    refactor fetch task to onCreate()

--- app/src/main/java/com/ilyarudyak/android/popmovies/fav/FavDetailFragment.java
+++ app/src/main/java/com/ilyarudyak/android/popmovies/fav/FavDetailFragment.java
@@ -3,9 +3,6 @@ package com.ilyarudyak.android.popmovies.fav;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.ilyarudyak.android.popmovies.DetailFragment;
 import com.ilyarudyak.android.popmovies.utils.FavDbUtils;
@@ -17,17 +14,14 @@ public class FavDetailFragment extends DetailFragment {
 
     public static final String TAG = FavDetailFragment.class.getSimpleName();
 
-
     public FavDetailFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         new FetchBundleTask().execute();
-        return super.onCreateView(inflater, container, savedInstanceState);
     }
-
 
     // ------------------ fetch mBundle ------------------
 
@@ -35,6 +29,7 @@ public class FavDetailFragment extends DetailFragment {
 
         @Override
         protected Void doInBackground(Void... params) {
+
             Uri movieUri = getActivity().getIntent().getParcelableExtra(
                     FavPosterFragment.DETAIL_URI);
 
